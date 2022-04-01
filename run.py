13258f471b4731225b7cf68f9fce4ac2ec904872
healygi <healygi@tcd.ie>
2022-04-01 12:33:20 +0000

    Create function to create board

--- run.py
+++ run.py
@@ -5,3 +5,5 @@ board = [[" "] * 8 for x in range(8)]
 #Creat variable to display misses and hits
 hidden_board =  [[" "] * 8 for i in range(8)]
 
+def create_board(board):
+    print(create_board(board))
