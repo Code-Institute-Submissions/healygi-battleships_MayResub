d61448c140712ba10824733dfd5659faf54a4502
healygi <healygi@tcd.ie>
2022-04-01 12:29:18 +0000

    Add variables to create  boards  and display misses and hits.

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from random import randint
 
-
+#Create variable to make board and store ships
+board = [[" "] * 8 for x in range(8)]
+#Creat variable to display misses and hits
+hidden_board =  [[" "] * 8 for i in range(8)]
 
