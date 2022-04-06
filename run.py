f8c11f515d2ed33a27fcac51d5e18b3c3e325563
healygi <healygi@tcd.ie>
2022-04-06 11:19:05 +0000

    Add greeting for user and input name

--- run.py
+++ run.py
@@ -6,12 +6,11 @@ board = [[" "] * 4 for x in range(8)]
 #Create variable to display misses and hits
 hidden_board =  [[" "] * 4 for i in range(8)]
 print(board)
-"""
 
 class Board:
 
     def __init__(self, size, ships, name, type):
-        """Initialize board attrubutes"""
+        "Initialize board attrubutes
         self.size = size
         self.num_ships = num_ships
         self.board = [["." for x in range(size)] for y in range(size)]
@@ -28,6 +27,20 @@ class Board:
         self.guesses.append((x, y))
         self.board[x][y] = "x"
 
+"""
+
+#Create greeting message for player
+
+greeting = "Welcome to battlships- play against the computer and be the first to sink all 5 of your opponent's ships."
+print(greeting)
+
+#Ask for players name
+
+name = input("What is your name Comrade? " )
+print("Hello " + name )
+print("lets set out to sea and destroy the computers ships!!")
+
+# Instructions on how to play
 
 
 
